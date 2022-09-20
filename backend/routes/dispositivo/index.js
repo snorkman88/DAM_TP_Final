0693f2cf63eea900d2a0eddb31c1d9d162d365b9
snorkman88 <milton.eduardo.sosa@gmail.com>
2022-09-20 19:16:08 +0100

    add gitignore

---
+++ backend/routes/dispositivo/index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var routerDispositivo = express.Router();
+var pool = require('../../mysql');
+
+//Devuelve un array de dispositivos
+routerDispositivo.get('/', function(req, res) {
+    pool.query('Select * from Dispositivos', function(err, result, fields) {
+        if (err) {
+            res.send(err).status(400);
+            return;
+        }
+        res.send(result);
+    });
+});
+
+routerDispositivo.get('/:idDispositivo', function(req, res) {
+    pool.query('Select * from Dispositivos where dispositivoId=?', [req.params.idDispositivo], function(err, result, fields) {
+        if (err) {
+            res.send(err).status(400);
+            return;
+        }
+        res.send(result[0]);
+    });
+});
+
+//Espera recibir por parámetro un id de dispositivo y devuelve todos los logs
+routerDispositivo.get('/:idDispositivo/logs', function(req, res) {
+    pool.query('Select * from Log_Riegos where electrovalvulaId=? order by fecha desc', [req.params.idDispositivo], function(err, result, fields) {
+        if (err) {
+            res.send(err).status(400);
+            return;
+        }
+        res.send(result);
+    });
+});
+
+//Espera recibir por parámetro un id de dispositivo y un valor de medición y lo inserta en base de datos.
+routerDispositivo.post('/:idDispositivo/agregarLog', function(req, res) {
+    pool.query('Insert into Log_Riegos (fecha,apertura,electrovalvulaId) values (?,?,?)', [req.body.fecha, req.body.apertura, req.params.idDispositivo], function(err, result, fields) {
+        if (err) {
+            res.send(err).status(400);
+            return;
+        }
+        res.send(result);
+    });
+});
+
+module.exports = routerDispositivo;
